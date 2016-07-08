f011509b5cfad8fe623d7ebde7ffa6b4903924e3
lucas nunes de sousa <lucasnshunter@hotmail.com>
2016-07-08 14:49:35 +0000

    classe controller static pages foi destruido

--- app/controllers/pdv_controller.rb
+++ app/controllers/pdv_controller.rb
@@ -1,18 +1,24 @@
 class PdvController < ApplicationController
   def dashboard
     @pdv=Pdv.new
+    @nome_da_view="Dashboard"
+    
   end
   
   def sell_water
+        @nome_da_view="Vender Água"
   end
 
   def sell_gas
+        @nome_da_view="Vender gás"
   end
 
   def sell_credit_cell
+        @nome_da_view="Vender credito de celular"
   end
 
   def show
+        @nome_da_view="Show"
   end
 
   
